b066a172fc48f948faf83e03c802742e1462a6f5
AnthoJaraBustamante <omarbush81@gmail.com>
2021-12-14 19:15:16 -0500

    .

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/user200526/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/user200526/Documents/APPS/interkontcondorapp"
+export "FLUTTER_ROOT=d:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\RODMENSOFT\interkont-condor-app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.1"
-export "FLUTTER_BUILD_NUMBER=3"
+export "FLUTTER_BUILD_NUMBER=4"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
