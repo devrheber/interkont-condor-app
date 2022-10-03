5f9ab922b8e1bff61c3553469ae7ce54da12a175
Jean Chiclla <j-c_f@outlook.com>
2022-10-03 14:10:03 -0500

    [FEATURE] Se agrega App Distribution de Firebase

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\USUARIO HP\fvm\versions\2.10.5"
-export "FLUTTER_APPLICATION_PATH=D:\RODMENSOFT\interkont-condor-app"
+export "FLUTTER_ROOT=/home/jean/fvm/versions/2.10.5"
+export "FLUTTER_APPLICATION_PATH=/home/jean/rodmensoft/interkont-condor-app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.1"
 export "FLUTTER_BUILD_NUMBER=4"
