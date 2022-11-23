bd496e0d0f5843464201e03bc135e04e2f25253e
Heber Rodríguez <devrheber@gmail.com>
2022-11-23 15:38:54 -0500

    nueva versión ios

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/jean/fvm/versions/3.0.1"
-export "FLUTTER_APPLICATION_PATH=/home/jean/rodmensoft/interkont_condor_app"
+export "FLUTTER_ROOT=/Users/user200526/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/user200526/Documents/APPS/interkontcondorapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.0.3"
-export "FLUTTER_BUILD_NUMBER=23"
+export "FLUTTER_BUILD_NAME=6.0.2"
+export "FLUTTER_BUILD_NUMBER=22"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
