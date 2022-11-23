6793ee6f7cfed7753c37f046d3341ddd8de39c56
Jean Chiclla <j-c_f@outlook.com>
2022-11-23 16:55:03 -0500

    [FIX] Modelo de Activos "GestionAom" Se borran algunas propiedades que no se usan.

--- lib/domain/models/gestion_aom.dart
+++ lib/domain/models/gestion_aom.dart
@@ -20,8 +20,6 @@ class GestionAom {
     required this.estadoSupervisorId,
     required this.estadoNombreSupervisor,
     required this.cantidad,
-    this.estadoInventarioId,
-    this.estadoNombreInventario,
     required this.estadoAomId,
     required this.estadoNombreAom,
     required this.updatedAt,
@@ -31,21 +29,11 @@ class GestionAom {
     required this.descripcionDetalle,
     required this.unidadId,
     required this.strNombreUnidad,
-    this.rutaInforme,
-    this.rutaPoliza,
-    this.rutaImagen,
-    required this.latitud,
-    required this.longitud,
-    required this.ubicacion,
     this.observacion,
     required this.valorInicial,
-    required this.vidaUtilRemanente,
     required this.anosRestantes,
-    this.kmlLineaUrl,
-    this.kmlPoligonoUrl,
     required this.tipoMapaActivo,
     required this.clasificacionRelacionObraActivos,
-    this.strNombreImagen,
     required this.operatividad,
   });
 
@@ -57,8 +45,6 @@ class GestionAom {
   final int estadoSupervisorId;
   final String estadoNombreSupervisor;
   final int cantidad;
-  final dynamic estadoInventarioId;
-  final dynamic estadoNombreInventario;
   final int estadoAomId;
   final String estadoNombreAom;
   final String updatedAt;
@@ -68,21 +54,11 @@ class GestionAom {
   final String descripcionDetalle;
   final int unidadId;
   final String strNombreUnidad;
-  final dynamic rutaInforme;
-  final dynamic rutaPoliza;
-  final dynamic rutaImagen;
-  final double latitud;
-  final double longitud;
-  final String ubicacion;
   final String? observacion;
   final double valorInicial;
-  final DateTime vidaUtilRemanente;
   final int anosRestantes;
-  final dynamic kmlLineaUrl;
-  final dynamic kmlPoligonoUrl;
   final int tipoMapaActivo;
   final int clasificacionRelacionObraActivos;
-  final dynamic strNombreImagen;
   final bool operatividad;
 
   factory GestionAom.fromJson(Map<String, dynamic> json) => GestionAom(
@@ -94,8 +70,6 @@ class GestionAom {
         estadoSupervisorId: json["estadoSupervisorId"],
         estadoNombreSupervisor: json["estadoNombreSupervisor"],
         cantidad: json["cantidad"].round(),
-        estadoInventarioId: json["estadoInventarioId"],
-        estadoNombreInventario: json["estadoNombreInventario"],
         estadoAomId: json["estadoAomId"],
         estadoNombreAom: json["estadoNombreAom"],
         updatedAt: json["updated_at"],
@@ -105,22 +79,12 @@ class GestionAom {
         descripcionDetalle: json["descripcionDetalle"],
         unidadId: json["unidadId"],
         strNombreUnidad: json["strNombreUnidad"],
-        rutaInforme: json["rutaInforme"],
-        rutaPoliza: json["rutaPoliza"],
-        rutaImagen: json["rutaImagen"],
-        latitud: json["latitud"].toDouble(),
-        longitud: json["longitud"].toDouble(),
-        ubicacion: json["ubicacion"],
         observacion: json["observacion"],
         valorInicial: json["valorInicial"],
-        vidaUtilRemanente: DateTime.parse(json["vidaUtilRemanente"]),
         anosRestantes: json["anosRestantes"].round(),
-        kmlLineaUrl: json["kmlLineaUrl"],
-        kmlPoligonoUrl: json["kmlPoligonoUrl"],
         tipoMapaActivo: json["tipoMapaActivo"],
         clasificacionRelacionObraActivos:
             json["clasificacionRelacionObraActivos"],
-        strNombreImagen: json["strNombreImagen"],
         operatividad: json['operatividad'],
       );
 
@@ -133,8 +97,6 @@ class GestionAom {
         "estadoSupervisorId": estadoSupervisorId,
         "estadoNombreSupervisor": estadoNombreSupervisor,
         "cantidad": cantidad,
-        "estadoInventarioId": estadoInventarioId,
-        "estadoNombreInventario": estadoNombreInventario,
         "estadoAomId": estadoAomId,
         "estadoNombreAom": estadoNombreAom,
         "updated_at": updatedAt,
@@ -144,22 +106,11 @@ class GestionAom {
         "descripcionDetalle": descripcionDetalle,
         "unidadId": unidadId,
         "strNombreUnidad": strNombreUnidad,
-        "rutaInforme": rutaInforme,
-        "rutaPoliza": rutaPoliza,
-        "rutaImagen": rutaImagen,
-        "latitud": latitud,
-        "longitud": longitud,
-        "ubicacion": ubicacion,
         "observacion": observacion,
         "valorInicial": valorInicial,
-        "vidaUtilRemanente":
-            "${vidaUtilRemanente.year.toString().padLeft(4, '0')}-${vidaUtilRemanente.month.toString().padLeft(2, '0')}-${vidaUtilRemanente.day.toString().padLeft(2, '0')}",
         "anosRestantes": anosRestantes,
-        "kmlLineaUrl": kmlLineaUrl,
-        "kmlPoligonoUrl": kmlPoligonoUrl,
         "tipoMapaActivo": tipoMapaActivo,
         "clasificacionRelacionObraActivos": clasificacionRelacionObraActivos,
-        "strNombreImagen": strNombreImagen,
         'operatividad': operatividad,
       };
 }
