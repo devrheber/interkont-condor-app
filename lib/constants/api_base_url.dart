70c4c6c570247d307fcd27aca0595407b3e624ed
Jean Chiclla <j-c_f@outlook.com>
2023-04-04 17:17:50 -0500

    [FIX] base url

--- lib/constants/api_base_url.dart
+++ lib/constants/api_base_url.dart
@@ -1,20 +1,15 @@
 // TODO Flavors
 
-// Avánzame
-// String _baseUrlPruebas = 'https://avanzamepruebas.minenergia.gov.co';
-// String _baseUrlProduccion = 'https://backendavanzame.minenergia.gov.co';
-
 String _baseUrlPruebas = 'http://13.59.62.87:7073/siente3-ws-consulta';
-String _baseUrlProduccion = 'https://sienteapp.gestiondelriesgo.gov.co';
-String _app = 'siente3-ws';
-// https://sienteapp.gestiondelriesgo.gov.co/siente3-ws
-String _baseUrl = '$_baseUrlProduccion/$_app';
+String _baseUrlProduccion = 'https://sienteapp.gestiondelriesgo.gov.co/siente3-ws';
 
-// String urlGlobalApiCondor = '$_baseUrl/$_app';
+// String _baseUrl = _baseUrlPruebas;
+String _baseUrl = _baseUrlProduccion;
+
 String urlGlobalApiCondor = _baseUrl;
 String urlApiAom = '$_baseUrl/avanzaweb-ws-AOM';
 String urlFiles = '$_baseUrl/files-ws';
 String urlBaseWeb = '$_baseUrl/avanzaweb-ws';
 
 // TODO
-String versionNumber = "2.0.3+3";
+String versionNumber = "2.0.5+5";
